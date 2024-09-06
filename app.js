3c9627e904a5da809fde6c46c507c466760df086
Irshad Alam <irshadalam3092006@gmail.com>
2024-09-06 18:40:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// JavaScript to handle future functionality
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to handle any features can be added here
+    console.log("Sahayak Prototype Loaded!");
+});
